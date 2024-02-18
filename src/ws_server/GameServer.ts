52b291971d441ca075c7561efe800f80352bceb5
Nikita <nikitosminsk100@gmail.com>
2024-02-18 22:22:43 +0300

    feat: game and room; testing

--- src/ws_server/GameServer.ts
+++ src/ws_server/GameServer.ts
@@ -1,12 +1,27 @@
 import WebSocket from 'ws';
-import { AuthData, Message, MessageType, RegResponseData, User } from './types';
+import {
+  AddToRoomData,
+  AuthData,
+  CreateGameResponseData,
+  Game,
+  Message,
+  RegResponseData,
+  Room,
+  User,
+  WithId,
+  WithUserIndex
+} from './types';
 import { uuidv4 } from './utils';
 
 
 export class GameServer {
-  private readonly users: Map<string, User> = new Map<string, User>();
-  private readonly connections: Map<string, WebSocket.WebSocket> = new Map<string, WebSocket.WebSocket>();
-  private webSocketServer: WebSocket.Server;
+  private usersIdCounter: number = 0;
+  private roomsIdCounter: number = 0;
+
+  private readonly users: User[] = [];
+  private readonly rooms: Room[] = [];
+  private readonly games: Game[] = [];
+  private webSocketServer!: WebSocket.Server;
   private isStarted: boolean = false;
 
   public constructor(private readonly port: number) {
@@ -30,7 +45,8 @@ export class GameServer {
 
     this.webSocketServer.on('connection', (client: WebSocket.WebSocket): void => {
       const id = uuidv4();
-      this.connections.set(id, client);
+
+      (client as WithId).id = id;
 
       client.on('connection', () => {
         console.log(`New client connected. Gave him ID: ${id}`);
@@ -39,22 +55,33 @@ export class GameServer {
       client.on('message', (message): void => {
         this.processClientMessage(client, message);
       });
-
-      client.on('close', () => {
-        this.connections.delete(id);
-      });
     });
   }
 
   private processClientMessage(client: WebSocket.WebSocket, message: WebSocket.RawData): void {
     try {
       const messageRawParsed: Message = JSON.parse(message.toString());
+      console.log(messageRawParsed);
 
       switch (messageRawParsed.type) {
         case 'reg': {
           const data: AuthData = JSON.parse(messageRawParsed.data);
           this.processReg(data, client);
           this.updateWinners();
+          this.sendFreeRooms();
+          break;
+        }
+        case 'create_room': {
+          this.createRoom(client as WithUserIndex);
+          this.sendFreeRooms();
+          break;
+        }
+        case 'add_user_to_room': {
+          const data: AddToRoomData = JSON.parse(messageRawParsed.data);
+          const roomIndex = this.addToRoom(client as WithUserIndex, data);
+          this.sendFreeRooms();
+          this.createGame(roomIndex);
+
           break;
         }
       }
@@ -66,20 +93,24 @@ export class GameServer {
   private static getUserPk(name: string, password: string): string {
     return name + password;
   }
+
   private processReg(messageData: AuthData, client: WebSocket.WebSocket): void {
     const userPk = GameServer.getUserPk(messageData.name, messageData.password);
 
-    if (!this.users.has(userPk)) {
+    let tryFind = this.users.find(u => GameServer.getUserPk(u.name, u.password) === userPk);
+
+    if (!this.users.find(u => GameServer.getUserPk(u.name, u.password) === userPk)) {
       const newUser: User = {
         ...messageData,
-        index: this.users.size,
+        index: this.usersIdCounter++,
         wins: 0
       };
 
-      this.users.set(userPk, newUser);
+      this.users.push(newUser);
+      tryFind = newUser;
     }
 
-    const user = this.users.get(userPk);
+    const user = tryFind as User;
 
     const responseRegData: RegResponseData = {
       name: user.name,
@@ -94,6 +125,7 @@ export class GameServer {
       id: 0
     };
 
+    (client as WithUserIndex).userIndex = user.index;
     client.send(JSON.stringify(responseReg));
   }
 
@@ -118,4 +150,89 @@ export class GameServer {
       client.send(stringified);
     });
   }
+
+  private createRoom(client: WithUserIndex): void {
+    const currentUser = this.users.find(u => u.index === client.userIndex);
+
+    if (!currentUser) {
+      return;
+    }
+
+    const newRoom: Room = {
+      roomUsers: [{name: currentUser.name, index: currentUser.index}],
+      index: this.roomsIdCounter++
+    };
+
+    this.rooms.push(newRoom);
+  }
+
+  private addToRoom(client: WithUserIndex, data: AddToRoomData): number {
+     const { indexRoom } = data;
+
+    const index = this.rooms.findIndex(r => r.index === indexRoom);
+
+    if (index < 0) {
+      throw Error('addToRoom: index not found');
+    }
+
+    const { userIndex } = client;
+
+    if (!userIndex) {
+      throw Error('NOT FOUND');
+    }
+
+    const currentUser = this.users.find(u => u.index === client.userIndex);
+
+    if (!currentUser) {
+      throw Error('NOT FOUND');
+    }
+
+    this.rooms[index].roomUsers.push({
+      name: currentUser.name,
+      index: currentUser.index
+    });
+
+    return index;
+  }
+
+  private sendFreeRooms(): void {
+    const free = this.rooms.filter(r => r.roomUsers.length === 1);
+    const stringified = JSON.stringify(free);
+    const response: Message = {
+      type: 'update_room', data: stringified, id: 0
+    };
+    const stringifiedResponse = JSON.stringify(response);
+
+    this.webSocketServer.clients.forEach(client => client.send(stringifiedResponse))
+  }
+
+  private createGame(roomIndex: number) {
+    const id = uuidv4();
+    const player1 = this.rooms[roomIndex].roomUsers[0].index;
+    const player2 = this.rooms[roomIndex].roomUsers[1].index;
+
+    this.games.push({id, player1, player2});
+
+    const data: CreateGameResponseData = {
+      idGame: id,
+      idPlayer: player2
+    };
+
+    const message: Message = {
+      type: 'create_game', id: 0, data: JSON.stringify(data)
+    };
+
+    const json = JSON.stringify(message);
+
+    const player1Client = this.findClient(player1);
+    const player2Client = this.findClient(player2);
+
+    player1Client?.send(json);
+    player2Client?.send(json)
+  }
+
+  private findClient(index: number): WebSocket.WebSocket | undefined {
+    return [...this.webSocketServer.clients.values()].find(ws => (ws as WithUserIndex).userIndex === index);
+  }
+
 }
--- src/ws_server/types.ts
+++ src/ws_server/types.ts
@@ -1,14 +1,39 @@
+import WebSocket from 'ws';
+
 export interface AuthData {
   readonly name: string;
   readonly password: string;
+}
+
+export interface WithUserIndex extends WebSocket.WebSocket {
+  userIndex: number;
+}
+
+export interface WithId extends WebSocket.WebSocket {
+  id: string;
 }
 
 export interface User extends AuthData {
   readonly index: number;
   wins: number;
 }
+export type UserInRoom = Pick<User, 'index'> & Pick<AuthData, 'name'>;
+export interface Room {
+  roomUsers: UserInRoom[];
+  index: number;
+}
 
-export type MessageType = 'reg' | 'create_game' | 'start_game' | 'turn' | 'attack' | 'finish' | 'update_room' | 'update_winners';
+export interface AddToRoomData {
+  indexRoom: number;
+}
+
+export interface Game {
+  readonly id: string;
+  player1: number;
+  player2: number;
+}
+
+export type MessageType = 'reg' | 'create_room' | 'add_user_to_room' | 'create_game' | 'add_ships' | 'start_game' | 'turn' | 'attack' | 'randomAttack' | 'finish' | 'update_room' | 'update_winners';
 
 export interface Message {
   readonly type: MessageType;
@@ -20,3 +45,9 @@ export interface RegResponseData extends Omit<User, 'password' | 'wins'> {
   readonly error: boolean;
   readonly errorText: string;
 }
+
+export interface CreateGameResponseData {
+  idGame: string;
+  idPlayer: number;
+}
+
